17d8b03be05ad99df89a123113c87b893db4ecac
silvia <silvia@air-di-silvia>
2018-05-28 18:42:44 +0200

    Aggiornato ultima versione db 25/05

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,112 @@
 package it.polito.tdp.libretto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Libretto {
+	
+	private List<Voto> voti;
+	
+	public Libretto() {
+		this.voti=new ArrayList<>();
+	}
+	
+	
+	public Libretto(List<Voto> voti) {
+	     //	this.voti=voti;
+	     //non è cambiato perchè prima ho copiato il reference, ovvero entrambi i libretti puntano allo stesso indirizzo di memoria
+		
+		//	this.voti=new ArrayList<Voto>(voti);
+		// sbagliata ancora, copio gli stessi voti
+		
+		
+		//faccio lo stesso di sopra
+//		this.voti=new ArrayList<Voto>();
+//		for(Voto v: voti) {
+//			this.voti.add(v);
+//		}
+		
+		
+		//oggetti identici che contengono gli stessi valori
+		this.voti=new ArrayList<Voto>();
+		for(Voto v:voti) {
+			this.voti.add(new Voto(v.getNome(), v.getVoto()));
+		}
+	}
+	
+	
+	
+	public void add(Voto v) {
+		if(this.voti.contains(v)) {
+			System.out.println("Il voto esiste già");
+			return;
+		}
+		
+		for(Voto voto: this.voti) {
+			if(voto.getNome().equals(v.getNome())) {
+				System.out.println("Trovato lo stesso esame (aggiornato il voto)");
+				voto.setVoto(v.getVoto());
+			}
+		}
+		this.voti.add(v);
+	}
+
+	public void stampa() {
+		// TODO Auto-generated method stub
+		System.out.println("Ci sono "+voti.size()+" voti ");
+		for(Voto v: this.voti) {
+			System.out.println(v);
+		}
+	}
+
+	public void stampa25() {
+		// TODO Auto-generated method stub
+		for(Voto v :this.voti) {
+			if(v.getVoto()==25) {
+				System.out.println(v);
+			}
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb= new StringBuilder();
+		sb.append("\nCi sono "+voti.size()+" voti\n");
+		
+		for(Voto v: this.voti) {
+			sb.append(v.toString());
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
+
+	public String getMarkfromClass(String nomeCorso) {
+		// TODO Auto-generated method stub
+		for(Voto v:this.voti) {
+			if(v.getNome().toLowerCase().equals(nomeCorso.toLowerCase())) {
+				return String.valueOf(v.getVoto());
+			}
+		}
+		return "nomeCorso not found";
+	}
+	
+	
+	public List<Voto> getVoti(){
+		return this.voti;
+	}
+
+
+	public void alzaVoti() {
+		// TODO Auto-generated method stub
+		for(Voto v:this.voti) {
+			if(v.getVoto()>=24) {
+				v.setVoto(v.getVoto()+2);
+			}else if(v.getVoto()>=18) {
+				v.setVoto(v.getVoto()+1);
+			}
+		}
+	}
+
 	
 }
--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -1,5 +1,72 @@
 package it.polito.tdp.libretto;
 
-public class Voto {
+public class Voto implements Comparable<Voto>{
+	
+	private String nome;
+	private int voto;
+	
+	//info temporali sono una rogna
+	//private LocalDate data;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public Voto(String nome, int voto) {
+		super();
+		this.nome = nome;
+		this.voto = voto;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		//per gesiìtire i valori errati
+		if(voto<18 || voto>34) {
+			throw new IllegalArgumentException("Valore non ammesso");
+		}
+		this.voto = voto;
+	}
+	@Override
+	public String toString() {
+		return "Voto [nome=" + nome + ", voto=" + voto + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + voto;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Voto other = (Voto) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (voto != other.voto)
+			return false;
+		return true;
+	}
+	@Override
+	public int compareTo(Voto v) {
+		// TODO Auto-generated method stub
+		return this.nome.compareTo(v.getNome());
+	}
+	
+
 	
 }
